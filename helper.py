1bbb3ba1a3628849f5920a11c561ed34e247abd8
Viileppi <vili.rasanen@gmail.com>
2018-03-21 15:09:08 +0200

    helper added

---
+++ helper.py
@@ -0,0 +1,25 @@
+def showHelp(scr):
+    l = [ "RPOGUENG - the testgame\n",
+        "You are exploring a previously unknown planet,",
+        "which is populated with all sorts of creatures.\n",
+        "Your mission is to make the red hostile aliens",
+        "non-hostile. This can be accomplished by either",
+        "talking to them (press 't') or in some cases",
+        "by hitting them... even to the death if needed.\n",
+        "To view the map, press 'm'. The map will show",
+        "areas you have explored. Areas with hostile ",
+        "aliens are shown in red. Press 'q' to quit.\n",
+        "Friendly green aliens will give you their HP",
+        "if you're wounded and they'll fight for you.\n",
+        "To move, use following keyset:\n",
+        " y  k  u",
+        "  \\ | /",
+        "h - @ - l",
+        "  / | \\",
+        " b  j  n"]
+    for line in l:
+        scr.addstr(line + "\n")
+    scr.refresh()
+    scr.getkey()
+    scr.clear()
+    scr.refresh()
